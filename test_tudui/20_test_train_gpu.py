c9146fe0277b6b6ddc00a1803b69f80963f7dc89
asuka158 <2993243876@qq.com>
2025-12-15 02:03:09 +0800

    train_by_gpu

---
+++ test_tudui/20_test_train_gpu.py
@@ -0,0 +1,115 @@
+import torchvision
+from torch.utils.data import DataLoader
+from torch import nn
+import torch
+# from model import *
+from torch.utils.tensorboard import SummaryWriter
+import time
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# 1. 准备数据集
+train_data = torchvision.datasets.CIFAR10(root="../dataset", train=True, transform=torchvision.transforms.ToTensor(), download=True)
+test_data = torchvision.datasets.CIFAR10(root="../dataset", train=False, transform=torchvision.transforms.ToTensor(), download=True)
+
+train_data_size = len(train_data)
+test_data_size = len(test_data)
+
+print("训练数据集的长度为:{}".format(train_data_size))
+print("测试数据集的长度为:{}".format(test_data_size))
+
+# 2. 利用 DataLoader 来加载数据集
+train_dataloader = DataLoader(train_data, batch_size=64)
+test_dataloader = DataLoader(test_data, batch_size=64)
+
+# 3. 创建网络模型
+class Tudui(nn.Module):
+    def __init__(self):
+        super(Tudui, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Flatten(),
+            nn.Linear(64*4*4, 64),
+            nn.Linear(64, 10)
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+tudui = Tudui()
+tudui.to(device)
+
+# 4. 损失函数
+loss_fn = nn.CrossEntropyLoss()
+loss_fn.to(device)
+
+# 5. 优化器
+learning_rate = 0.01
+optimizer = torch.optim.SGD(tudui.parameters(), lr=learning_rate)
+
+# 6. 设置训练网络的一些参数
+# 记录训练的次数
+total_train_step = 0
+# 记录测试的次数
+total_test_step = 0
+# 训练的轮数
+epoch = 10
+
+# 添加tensorboard
+writer = SummaryWriter("./logs")
+
+start_time = time.time()
+for i in range(epoch):
+    print("------第{}轮训练开始------".format(i+1))
+    
+    # 训练步骤开始
+    tudui.train()
+    for data in train_dataloader:
+        imgs, targets = data
+        imgs = imgs.to(device)
+        targets = targets.to(device)
+        outputs = tudui(imgs)
+        loss = loss_fn(outputs, targets)
+
+        # 优化器优化模型
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        total_train_step = total_train_step + 1
+        if total_train_step % 100 == 0:
+            end_time = time.time()
+            print("训练{}次所需时间：{}s".format(total_train_step, end_time - start_time)) # 运行训练一百次后的时间间隔
+            print("训练次数:{}, Loss: {}".format(total_train_step, loss.item()))
+            writer.add_scalar("train_loss", loss.item(), total_train_step)
+
+    # 测试步骤开始
+    tudui.eval()
+    total_test_loss = 0
+    total_accuracy = 0
+    with torch.no_grad():
+        for data in test_dataloader:
+            imgs, targets = data
+            imgs = imgs.to(device)
+            targets = targets.to(device)
+            outputs = tudui(imgs)
+            loss = loss_fn(outputs, targets)
+            total_test_loss = total_test_loss + loss
+            accuracy = (outputs.argmax(1) == targets).sum()
+            total_accuracy += accuracy
+    print("整体测试集上的Loss: {}".format(total_test_loss))  
+    print("整体测试集上的正确率: {}".format(total_accuracy/test_data_size))   
+    writer.add_scalar("test_loss", total_test_loss, total_test_step)  
+    writer.add_scalar("test_accuracy", total_accuracy/test_data_size, total_test_step)  
+    total_test_step += 1
+
+    torch.save(tudui, "tudui_{}.pth".format(i))
+    print("模型已保存")
+
+writer.close()    
